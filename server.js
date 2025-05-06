f162c9d81d42bb77dcd141c6fff7e57e0ef09089
lllMISHRAlll <soumyaranjanmishra29@gmail.com>
2025-05-06 21:08:14 +0530

    cors update 4

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectToDB();
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 app.use("/api/user", authRouter);
 app.use("/api/productpage", productRouter);

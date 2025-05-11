cef03feb7f5078ffc232f9614f91841160217263
lllMISHRAlll <soumyaranjanmishra29@gmail.com>
2025-05-11 20:55:02 +0530

    cors modified

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectToDB();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(bodyParser.json());
 
 app.use("/api/user", authRouter);
